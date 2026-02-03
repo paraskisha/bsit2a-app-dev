c39a896198bdb2a2d3afaea454fcd73d06ee2e81
paraskisha <paraskisha08@gmail.com>
2026-02-03 10:23:44 +0800

    Added layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <v-btn>Submit</v-btn>
+    
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-card height="80" color="blue">
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="80" color="pink">
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="80" color="purple"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="80" color="red"></v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
